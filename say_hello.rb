1f2fb86a81942ba74e38b47dc8b97b538757c5e7
Adella <adellakenn@gmail.com>
2019-04-22 22:25:39 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,8 +1,7 @@
 # Build your say_hello method here
 
-def say_hello(name)
+def say_hello(name = "Ruby Programmer")
   puts "Hello #{name}!"
-  puts "Hello Ruby Programmer"
 end
 
 say_hello("Kent Beck")
